e649ab1d26529bad3f4e3c2b516928b372af1ab0
cypherHades <xjadezbix@gmx.com>
2024-01-01 16:37:21 -0500

    [FEATURE]: Check for winner.

---
+++ app.js
@@ -0,0 +1,37 @@
+  function checkBoard() {
+    if (board[0][0] === "X" && board[0][1] === "X" && board[0][2] === "X") {
+      playerWins()
+    } else if (board[1][0] === "X" && board[1][1] === "X" && board[1][2] === "X") {
+      playerWins()
+    } else if (board[2][0] === "X" && board[2][1] === "X" && board[2][2] === "X") {
+      playerWins()
+    } else if (board[0][0] === "X" && board[1][0] === "X" && board[2][0] === "X") {
+      playerWins()
+    } else if (board[0][1] === "X" && board[1][1] === "X" && board[2][1] === "X") {
+      playerWins()
+    } else if (board[0][2] === "X" && board[1][2] === "X" && board[2][2] === "X") {
+      playerWins()
+    } else if (board[0][0] === "X" && board[1][1] === "X" && board[2][2] === "X") {
+      playerWins()
+    } else if (board[0][2] === "X" && board[1][1] === "X" && board[2][0] === "X") {
+      playerWins()
+    } else if (board[0][0] === "O" && board[0][1] === "O" && board[0][2] === "O") {
+      computerWins()
+    } else if (board[1][0] === "O" && board[1][1] === "O" && board[1][2] === "O") {
+      computerWins()
+    } else if (board[2][0] === "O" && board[2][1] === "O" && board[2][2] === "O") {
+      computerWins()
+    } else if (board[0][0] === "O" && board[1][0] === "O" && board[2][0] === "O") {
+      computerWins()
+    } else if (board[0][1] === "O" && board[1][1] === "O" && board[2][1] === "O") {
+      computerWins()
+    } else if (board[0][2] === "O" && board[1][2] === "O" && board[2][2] === "O") {
+      computerWins()
+    } else if (board[0][0] === "O" && board[1][1] === "O" && board[2][2] === "O") {
+      computerWins()
+    } else if (board[0][2] === "O" && board[1][1] === "O" && board[2][0] === "O") {
+      computerWins()
+    } else {
+      draw()
+    }
+  }
